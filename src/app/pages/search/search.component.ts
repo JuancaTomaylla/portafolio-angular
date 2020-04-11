cb96ff5b83de5ed1c033e71beba4f4d0fcf1bda6
Juanca Tomaylla <juancarlostb2@gmail.com>
2020-04-10 20:12:49 -0500

    Final Seccion 7

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
 
 @Component({
   selector: 'app-search',
@@ -8,7 +9,8 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SearchComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              public productoService: ProductosService) { }
 
   ngOnInit(): void {
 
@@ -16,6 +18,7 @@ export class SearchComponent implements OnInit {
     .subscribe(params => {
 
       console.log(params['termino'])
+      this.productoService.buscarProducto( params['termino']);
     });
   }
 
